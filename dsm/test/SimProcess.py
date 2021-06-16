f7370f862895ecccf77aa9e817541c20ccba9a84
eshivers <ersh7574@colorado.edu>
2021-06-16 09:21:37 +0500

    Started working on the Sim_Process file

--- dsm/test/SimProcess.py
+++
@@ -1,7 +0,0 @@
-"""
-Implement a node class that runs and subscribtes to the network by importing the claasses 
-of Arbiter and Memory Manager both of which are responsible for different part of the
-task in order to test whether or not the whole process is working correctly. This class
-will be used in the file __init__.py within the same folder
-"""
-
--- dsm/test/__init__.py
+++ dsm/test/__init__.py
@@ -1,17 +1,22 @@
-from varname import varname, nameof
+from .Sim_Process import Sim_Process
 
-class Song:
-    def __init__(self,name,artist):
-        self.name = name
-        self.artist = artist
+# from varname import varname, nameof
+
+# class Song:
+#     def __init__(self,name,artist):
+#         self.name = name
+#         self.artist = artist
     
 
-def main(var):
-    print(nameof(var))
+# def main(var):
+#     print(nameof(var))
+
+# if __name__ == "__main__":
+#     bruh = Song("In the End","Linkin Park")
+#     bruh1 = bruh
+#     bruh1.artist = "LP Family"
+#     print("New: ",bruh1.artist)
+#     print("Original: ",bruh.artist)
 
 if __name__ == "__main__":
-    bruh = Song("In the End","Linkin Park")
-    bruh1 = bruh
-    bruh1.artist = "LP Family"
-    print("New: ",bruh1.artist)
-    print("Original: ",bruh.artist)
+    proccess_1 = Sim_Process(1,15)
